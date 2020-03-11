136c4ba9ece7c401d70660664cbb974ae40f5fc0
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-03-12 00:45:28 +0300

    config

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?
 require __DIR__ . '/vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 $log = new Monolog\Logger('name');
 $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
 
@@ -8,3 +11,4 @@ $log->log('INFO', 'Started: '.$_ENV['Test']);
 
 echo "Hello world (php)!\n";
 echo $_ENV['Test'];
+phpinfo();
