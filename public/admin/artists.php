d41b06ce4b3581c7e2fe51de2c4d46be5693e55e
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-05-18 16:09:52 +0300

    refactor/cleanup

--- public/admin/artists.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-require_once __ROOT__ . '/src/utils/include.php';
-require_once __ROOT__ . '/src/dto/artist.php';
-
-use Somatik\Artist;
-
-render('artists.php', 'Artists', ['artists' => Artist::getAll()]);
--- templates/admin/login.php
+++
@@ -1,5 +0,0 @@
-<form action="admin" method="POST" style="margin: auto auto">
-    <label>Login <input type="text" name="login"></label>
-    <label>Password <input type="password" name="password"></label>
-    <button type="submit">Om WiFi</button>
-</form>
