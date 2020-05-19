6b9e6d4b411720b4bc2e885a6720f6dd6254ead5
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-05-19 14:13:04 +0300

    minor

--- src/db.php
+++ src/db.php
@@ -6,10 +6,9 @@ $dbname = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $password = getenv('DB_PASS');
 
-global $DBH;
 try {
     # MySQL через PDO_MYSQL
-    $DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $DBH = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
     $DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     error_log('Failed to connect to database: ' . $e->getMessage());
