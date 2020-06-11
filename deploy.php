062856a2c97f8082e0c8dea844e3640a55799818
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-06-11 22:13:54 +0300

    add npm i and npm run build

--- deploy.php
+++ deploy.php
@@ -31,14 +31,38 @@ host('somatiksoundsystem.com')
 
 // Tasks
 
-task('build', function () {
-    run('cd {{release_path}} && build');
+task('npm:install', function () {
+    run('cd {{release_path}}/static && npm i');
+});
+
+task('npm:run:build', function () {
+    run('cd {{release_path}}/static && npm run build');
+});
+
+desc('Deploy your project');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'npm:install',
+    'npm:run:build',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
+    'artisan:storage:link',
+    'artisan:view:cache',
+    'artisan:config:cache',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+]);
+
+set('static_path', function () {
+    return run('cd ./static && pwd');
 });
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
-
-// Migrate database before symlink new release.
-
-//before('deploy:symlink', 'artisan:migrate');
 
